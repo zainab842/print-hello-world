4fdf3459eb59956ad61882d9e6c77aa1be2d3bfa
Zainab <zanzakmbk2002@gmail.com>
2026-02-13 06:52:37 -0800

    first

--- datetime.py
+++ datetime.py
@@ -1,9 +1,3 @@
-x=5
-y=9
-z=8
-if(x>y):
-    print("x bigger then y")
-elif (x>z):
-    print("x bigger then z")
-else:
-    print("y bigger then x")
+import datetime
+x=input(datetime.datetime)
+print=(x)
