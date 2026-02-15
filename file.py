30c62497b02c6a65b9ee9dfc9447b93fdbed8cd0
Zainab <zanzakmbk2002@gmail.com>
2026-02-15 02:14:56 -0800

    first

--- file.py
+++ file.py
@@ -1,3 +1,16 @@
 x=open("zainab","w")
 x.write("hello world")
 x.close()
+
+x=open("zainab","r")
+print(x.read())
+x.close()
+
+a=open("data","w")
+a.write("python is fun and easy")
+a.close()
+a=open("data","r")
+print(a.tell())
+print(a.read(6))
+print(a.tell())
+a.close()
